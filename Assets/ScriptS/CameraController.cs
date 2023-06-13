4657f1c9411c140e750fa2e1a07c0fe9ac022a56
Stellar-Spire <136394794+Stellar-Spire@users.noreply.github.com>
2023-06-13 10:09:34 +1000

    FirstPush

---
+++ Assets/ScriptS/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set the offset of the camera based on the player
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Get the camera transform position to be that of the players tranform position
+        transform.position = player.transform.position + offset;
+    }
+}
