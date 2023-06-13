57bd81982045901a06c4c31d8a10202328d6a0eb
Stellar-Spire <136394794+Stellar-Spire@users.noreply.github.com>
2023-06-13 11:42:05 +1000

    Added Pick Ups

---
+++ Assets/ScriptS/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody rb;
+    private int pickupCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Get the number of pickups in our scene
+        pickupCount = GameObject.FindGameObjectsWithTag("Pick Up").Length;
+        //Run the check pickups function
+        CheckPickups();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.tag == "Pick Up")
+        {
+            Destroy(other.gameObject);
+            //Dectrement the pickup count
+            pickupCount -= 1;
+            //Run the check pickups function
+            CheckPickups();
+        }
+    }
+
+    void CheckPickups()
+    {
+        //Print the amount of pickups left in our scene
+        print("Pickups Left: " + pickupCount);
+
+        if (pickupCount == 0)
+        {
+            print("Win");
+        }
+    }
+}
